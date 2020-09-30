d5030e788f408be7c2543f0742d8583dfa3d31d1
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-09-30 21:38:13 +0200

    PWM requires values between 0 and 255

--- robot/Motor.ts
+++ robot/Motor.ts
@@ -33,12 +33,12 @@ export default function (in1: number, in2: number, ena: number): Motor {
           motor.in2.digitalWrite(0)
           motor.forward = true
         }
-        motor.ena.pwmWrite(Math.abs(speed))
+        motor.ena.pwmWrite(Math.abs(speed) * 2.55)
       }
 
       this.break = false
       if (speed !== this.speed) {
-        const diff = Math.min(5, Math.abs(speed - this.speed))
+        const diff = Math.min(20, Math.abs(speed - this.speed))
         sendSpeed(this, this.speed + Math.sign(speed - this.speed) * diff)
         setTimeout(() => this.accelerate.bind(this)(speed), 100)
       }
