48127c6b3a6d9875bc96bdbf6a9e88733c057e4c
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-10-01 22:37:21 +0200

    Loop starts immediately

--- robot/index.ts
+++ robot/index.ts
@@ -2,14 +2,6 @@ import Motor from './Motor'
 import Car, { Direction } from './Car'
 const Gpio = require('../gpio')
 
-// const led = new Gpio(19, {mode: Gpio.OUTPUT})
-
-// let value = 0
-// setInterval(() => {
-//   led.write(value)
-//   value = 1 - value
-// }, 500)
-
 const motor1 = Motor(2, 3, 4)
 const motor2 = Motor(17, 27, 22)
 const car = Car({ left: motor1, right: motor2 })
@@ -18,17 +10,19 @@ async function wait(millseconds: number) {
   return new Promise(resolve => setTimeout(resolve, millseconds))
 }
 
-const loop = setInterval(async () => {
-  car.accelerate(50)
+async function loop() {
+  car.accelerate(-50)
   await wait(500)
   car.turn(0, Direction.left)
   await wait(12000)
   car.stop()
-}, 12000)
+  setImmediate(loop)
+}
+
+loop()
 
 process.on('SIGINT', function() {
   console.log("Caught interrupt signal")
-  clearInterval(loop)
   car.stop()
   process.exit
 })
