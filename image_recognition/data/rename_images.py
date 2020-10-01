b9b9e0800893507b9f7bc00233ccff903b994307
Quajak <jasper.gerigk@gmx.net>
2020-10-01 22:52:58 +0200

    Fixed perf test and added file to rename images

---
+++ image_recognition/data/rename_images.py
@@ -0,0 +1,18 @@
+# Rename all images which simply have a single number to a better name
+from os import listdir, rename
+from os.path import isfile, join
+import random
+import string
+import re
+
+to_replace_filter = r"^\d+\.png"
+dir_path = r"./all_images/"
+files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+base_name = input("Base file name:")
+count = 0
+for file in files:
+    if re.search(to_replace_filter, file):
+        count += 1
+        rename(dir_path + file, dir_path + base_name + ''.join(random.choices(string.ascii_uppercase + string.digits, k=6)) + ".png")
+print("Renamed ", count, " out of ", len(files))
+print("Finished renaming")
--- robot/camera_perf_test.py
+++ robot/camera_perf_test.py
@@ -1,16 +1,24 @@
 from picamera import PiCamera
 from pathlib import Path
+import time
+import os
 
 root_dir = "./../pictures/test/"
 Path(root_dir).mkdir(parents=True, exist_ok=True)
 
+filelist = [ f for f in os.listdir(root_dir) ]
+for f in filelist:
+    os.remove(os.path.join(root_dir, f))
+
+num = int(input("Number of images to take"))
+
 if __name__ == "__main__":
     camera = PiCamera()
-    print("Taking a hundred pictures to time how long it takes")
+    print("Taking a " + str(num) + " pictures to time how long it takes")
     start = time.time()
     
-    for i in range(100):
+    for i in range(num):
         camera.capture(root_dir + str(i) + ".png")
 
     end = time.time()
-    print("100 pictures: ", end- start)
+    print(num, " pictures: ", end - start)
