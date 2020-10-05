7e3ed2f72a3d135dc50ba4e3eb9514b038e6d3d0
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-10-05 15:11:14 +0200

    List of available commands

--- robot/index.ts
+++ robot/index.ts
@@ -1,7 +1,7 @@
 import Motor from './Motor'
 import Car, { Direction } from './Car'
 import wait from './wait'
-const Gpio = require('../gpio')
+import { basename } from 'path'
 
 const motor1 = Motor(2, 3, 4)
 const motor2 = Motor(17, 27, 22)
@@ -18,11 +18,38 @@ async function loop() {
   await car.turn(180, Direction.left, 50, true)
 }
 
-async function turnOnSpot() {
-  await car.turn(360, Direction.left, 100, true)
+const commands = {
+  async loop() {
+    console.log('turn left 45°')
+    await car.turn(45, Direction.left, 50)
+    console.log('turn right 270°')
+    await car.turn(270, Direction.right, 50)
+    console.log('turn left 45°')
+    await car.turn(45, Direction.left, 50)
+    console.log('turn left 180° on the spot')
+    await car.turn(180, Direction.left, 50, true)
+  },
+  
+  async turnOnSpot() {
+    await car.turn(360, Direction.left, 100, true)
+  },
+  
+  async forward() {
+    await car.accelerate(50)
+    await wait(500)
+  },
+  
+  async back() {
+    await car.accelerate(-50)
+    await wait(500)
+  },
 }
 
-loop().then(car.stop)
+if (!process.argv[2]) {
+  console.error('\nUsage: ' + basename(process.argv.join(' ')) + ' <command>\nwith <command> equal to one of\n\t' + Object.keys(commands).join('\n\t'))
+} else {
+  commands[process.argv[2]].then(car.stop)
+}
 
 process.on('SIGINT', function() {
   console.log("Caught interrupt signal")
