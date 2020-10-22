77758125e64ee4e70e8a06cdc8c0d6480f724129
Quajak <jasper.gerigk@gmx.net>
2020-10-22 23:36:15 +0200

    Changed to socketio

--- image_recognition/use_classifier.py
+++ image_recognition/use_classifier.py
@@ -9,11 +9,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from classifier_net import Net
-import asyncio
-import websockets
 import atexit
 import cv2
 from pathlib import Path
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://localhost:2001')
+sio.wait()
 
 NET = '../class_net.pth'
 images = "./pictures/all_images/"
@@ -37,26 +53,22 @@ t = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0
 
 print("Loaded net")
 
-async def main_loop(uri):
-    async with websockets.connect(uri) as websocket:
-        print("Initialised websocket connection")
-        counter = 0
-        while True:
-            tstep = time()
-            ret, frame = cap.read()
-            frame = cv2.flip(frame, 0)
-            frame = cv2.flip(frame, 1)
-            cv2.imwrite(root_dir + str(counter) + '.png', frame)
-            img = io.imread(root_dir + str(counter) + '.png')
-            counter += 1
-            img = transform.resize(img, (72, 128))
-            img = t(img).float()
-            output = net.forward(img.unsqueeze(0))[0][0]
-            await websocket.send(str(output > 0.9))
-            print(output)
-            print("Took: " + str(time() - tstep))
-            tstep = time()
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(
-        main_loop('ws://localhost:80'))
+    print("Initialised websocket connection")
+    counter = 0
+    while True:
+        tstep = time()
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 0)
+        frame = cv2.flip(frame, 1)
+        cv2.imwrite(root_dir + str(counter) + '.png', frame)
+        img = io.imread(root_dir + str(counter) + '.png')
+        counter += 1
+        img = transform.resize(img, (72, 128))
+        img = t(img).float()
+        output = net.forward(img.unsqueeze(0))[0][0]
+        sio.emit(str(output > 0.9))
+        print(output)
+        print("Took: " + str(time() - tstep))
+        tstep = time()
