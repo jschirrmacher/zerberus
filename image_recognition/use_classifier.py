418a0da724ff4237085cef90ddfdaf6ac04e0204
Quajak <jasper.gerigk@gmx.net>
2020-10-22 21:08:05 +0200

    Updated all use_classifier

--- image_recognition/use_classifier.py
+++ image_recognition/use_classifier.py
@@ -8,7 +8,43 @@ import torch.nn.functional as F
 import torch.optim as optim
 from classifier_net import Net
 
-PATH = './class_net.pth'
+NET = '../class_net.pth'
+images = "./pictures/all_images/"
+Path(images).mkdir(parents=True, exist_ok=True)
+
+print("Setup file structure")
+
+cap = cv2.VideoCapture(0)
+
+def exit_handler():
+    cap.release()
+
+atexit.register(exit_handler)
+
+print("Initialised camera")
 
 net = Net()
-net.load_state_dict(torch.load(PATH))
+net.load_state_dict(torch.load(NET))
+
+t = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+print("Loaded net")
+
+if __name__ == "__main__":
+    counter = 0
+    while True:
+        tstep = time()
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 0)
+        frame = cv2.flip(frame, 1)
+        cv2.imwrite(root_dir + str(counter) + '.png', frame)
+        img = io.imread(root_dir + str(counter) + '.png')
+        counter += 1
+        img = transform.resize(img, (72, 128))
+        img = t(img).float()
+        print("Got picture in " + str(time() - tstep))
+        tstep = time()
+        output = net.forward(img.unsqueeze(0))
+        print(output)
+        print("Analysed picture in " + str(time() - tstep))
+        tstep = time()
