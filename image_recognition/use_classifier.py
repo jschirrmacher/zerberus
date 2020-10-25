fa8e27f8288b46675d41cb5210e4643c7fba9611
Quajak <jasper.gerigk@gmx.net>
2020-10-25 22:03:56 +0100

    Refractored + added missing io

--- image_recognition/use_classifier.py
+++ image_recognition/use_classifier.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import io
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -15,47 +15,26 @@ from pathlib import Path
 import socketio
 from time import time
 
-sio = socketio.Client()
-
 @sio.event
 def connect():
     print('connection established')
 
-@sio.event
-def my_message(data):
-    print('message received with ', data)
+    NET = '../class_net.pth'
+    images = "./pictures/all_images/"
+    Path(images).mkdir(parents=True, exist_ok=True)
 
-@sio.event
-def disconnect():
-    print('disconnected from server')
+    print("Setup file structure")
 
-sio.connect('ws://localhost:10000')
-print("Initialised websocket connection")
+    cap = cv2.VideoCapture(0)
 
-NET = '../class_net.pth'
-images = "./pictures/all_images/"
-Path(images).mkdir(parents=True, exist_ok=True)
+    print("Initialised camera")
 
-print("Setup file structure")
+    net = Net()
+    net.load_state_dict(torch.load(NET))
 
-cap = cv2.VideoCapture(0)
+    t = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-def exit_handler():
-    cap.release()
-
-atexit.register(exit_handler)
-
-print("Initialised camera")
-
-net = Net()
-net.load_state_dict(torch.load(NET))
-
-t = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-print("Loaded net")
-
-
-if __name__ == "__main__":
+    print("Loaded net")
     counter = 0
     while True:
         tstep = time()
@@ -72,3 +51,22 @@ if __name__ == "__main__":
         print(output)
         print("Took: " + str(time() - tstep))
         tstep = time()
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+if __name__ == "__main__":
+    def exit_handler():
+        cap.release()
+
+    atexit.register(exit_handler)
+
+    sio = socketio.Client()
+
+    sio.connect('ws://localhost:10000')
+    print("Initialised websocket connection")
