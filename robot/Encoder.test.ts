6c36ea48f951736d83a6ea0b40783525fb4a2ab6
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-11-22 07:13:51 +0100

    Refactored GPIOMock out of Encoder

--- robot/Encoder.test.ts
+++ robot/Encoder.test.ts
@@ -1,51 +1,7 @@
 import 'should'
-import { Stream } from 'stream'
 import EncoderFactory, { Encoder, TICKS_PER_REV } from "./Encoder"
-import { GPIO, GPIONotifier, GPIOPin, INPUT, ListenerFunction, PI_NTFY_FLAGS_ALIVE } from './gpio'
-
-const initializedPins = {}
-const listeners = []
-let currentValue = undefined
-const readableData = []
-
-const gpio: GPIO = {
-  create(pin: number, options: { mode: string }): GPIOPin {
-    initializedPins[pin] = options
-    return {
-      digitalWrite(value: number): void {
-        currentValue = value
-      },
-      pwmWrite(dutyCycle: number): void {
-        currentValue = dutyCycle
-      }
-    }
-  },
-
-  createNotifier(): GPIONotifier {
-    return {
-      simulated: true,
-
-      stream() {
-        return new Stream.Readable({
-          read() {
-            const value = readableData.shift()
-            value && this.push(value)
-          },
-          objectMode: true,
-        })
-      }
-    }
-  },
-
-  addListener(listener: ListenerFunction): number {
-    listeners.push(listener)
-    return listeners.length
-  },
-
-  removeListener(listenerId: number): void {
-    delete listeners[listenerId - 1]
-  },
-}
+import { INPUT, PI_NTFY_FLAGS_ALIVE } from './gpio'
+import { gpio, initializedPins, pushStreamData } from './GPIOMock'
 
 describe('Encoder', () => {
   let encoder: Encoder
@@ -127,13 +83,7 @@ describe('Encoder', () => {
   })
 
   function testStream(info: {flags: number, time: number, level: number}[]): Promise<void> {
-    const data = Buffer.alloc(12 * info.length)
-    info.forEach((entry, index) => {
-      data.writeInt16LE(entry.flags, index * 12 + 2)
-      data.writeInt32LE(entry.time, index * 12 + 4)
-      data.writeInt32LE(entry.level, index * 12 + 8)
-    })
-    readableData.push(data)
+    pushStreamData(info)
     return new Promise(resolve => setImmediate(resolve))
   }
 
