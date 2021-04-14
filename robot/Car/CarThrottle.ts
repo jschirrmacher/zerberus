28877edc01decf6fb7521b17aefbda60a1cf3cc6
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-04-14 05:34:16 +0200

    Use readable return type

--- robot/Car/CarThrottle.ts
+++ robot/Car/CarThrottle.ts
@@ -1,10 +1,11 @@
 type JoyStickValues = { x: number; y: number }
+type MotorThrottles = { left: number; right: number }
 
 function squarePercent(value: number) {
   return Math.sign(value) * Math.round((value / 100) * (value / 100) * 100)
 }
 
-export function throttleFromJoystickValues(value: JoyStickValues) {
+export function throttleFromJoystickValues(value: JoyStickValues): MotorThrottles {
   const x = squarePercent(value.x)
   const y = squarePercent(value.y)
   const offset = Math.max(y + x - 100, 0) + Math.min(y + x + 100, 0)
