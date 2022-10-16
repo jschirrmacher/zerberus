b5cf09a6fbf5d9a6cffbc5dceb7021c187deffaa
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-10-16 18:47:22 +0200

    No need to split already separate values

--- frontend/main.js
+++ frontend/main.js
@@ -74,9 +74,9 @@ import { CLIENT_TYPE } from "./types.js"
     const accel = msg.mpu?.accel.split(",")
     const gyro = msg.mpu?.gyro.split(",")
     const speed = msg.mpu?.speed.split(",")
-    mpuAccel.innerHTML = "<span>accel:</span><span>" + accel.split(",").join("</span><span>") + "</span>"
-    mpuGyro.innerHTML = "<span>gyro:</span><span>" + gyro.split(",").join("</span><span>") + "</span>"
-    mpuSpeed.innerHTML = "<span>speed:</span><span>" + speed.split(",").join("</span><span>") + "</span>"
+    mpuAccel.innerHTML = "<span>accel:</span><span>" + accel.join("</span><span>") + "</span>"
+    mpuGyro.innerHTML = "<span>gyro:</span><span>" + gyro.join("</span><span>") + "</span>"
+    mpuSpeed.innerHTML = "<span>speed:</span><span>" + speed.join("</span><span>") + "</span>"
 
     flightindicator.y1 = 75 + accel.y * 75
     flightindicator.y2 = 75 + accel.y * 75
