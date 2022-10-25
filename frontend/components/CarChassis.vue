9ab01ef8293390deb0177332d958f6e7c5f89102
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-10-25 06:53:52 +0200

    Car relative to zero origin

--- frontend/components/CarChassis.vue
+++ frontend/components/CarChassis.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import ABox from "./ABox.vue"
+
+defineProps<{
+  length: number
+  width: number
+  height: number
+  color: string
+}>()
 </script>
 
 <template>
-  <ABox class="car-chassis" length="500px" width="250px" height="125px" color="#000000" />
+  <ABox class="car-chassis" :length="length + 'px'" :width="width + 'px'" :height="height + 'px'" :color="color" />
 </template>
 
 <style scoped lang="scss"></style>
