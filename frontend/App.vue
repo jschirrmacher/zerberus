c5b32d324e695ef7445b2a6e0605b011aec544b7
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-10-30 12:51:55 +0100

    Shows car orientation and motor speeds

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,13 +1,38 @@
 <script setup lang="ts">
 import ThreeDeeWorld from "./components/ThreeDeeWorld.vue"
 import TheCar from "./components/TheCar.vue"
+import io from "socket.io-client"
+import { ref } from "vue"
+
+const socket = io("http://localhost:10000")
+
+const direction = ref(0)
+const leftMotorSpeed = ref(0)
+const rightMotorSpeed = ref(0)
+
+type MotorInfo = { throttle: number }
+type CarInfo = {
+  orientation: number
+  leftMotor: MotorInfo
+  rightMotor: MotorInfo
+}
+
+socket.on("hi", () => {
+  socket.emit("hi", ["REMOTE_CONTROL", "COCKPIT"])
+})
+
+socket.on("car-position", ({ orientation, leftMotor, rightMotor }: CarInfo) => {
+  direction.value = orientation
+  leftMotorSpeed.value = leftMotor.throttle
+  rightMotorSpeed.value = rightMotor.throttle
+})
 </script>
 
 <template>
   <header>Zerberus</header>
 
   <ThreeDeeWorld>
-    <TheCar :motor-speed-left="20" :motor-speed-right="100" :direction="45" />
+    <TheCar :motor-speed-left="leftMotorSpeed" :motor-speed-right="rightMotorSpeed" :direction="direction" />
   </ThreeDeeWorld>
 </template>
 
